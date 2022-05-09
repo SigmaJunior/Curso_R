621e7036cd5761b699ffbb982d3d4e9c162403aa
VitorBSP <vitorpereira3115@gmail.com>
2022-05-09 18:51:10 -0300

    aula 2 ended

--- aula2.R
+++ aula2.R
@@ -32,6 +32,65 @@ as.integer(curso)
 #Importando dados
 dados <- read.table('docs/Aula2/dados.txt')
 dados <- read.table('docs/Aula2/dados.txt', h = TRUE)
-dados <- read.table('docs/Aula2/dados.txt', h = TRUE,fileEncoding='UTF-8')
+dados <- read.table('docs/Aula2/dados.txt', h = TRUE, fileEncoding='UTF-8')
+
+# Alisson foi forçado pelo SERASA a economizar 1500 reais com skins em jogos online
+
+dados[2,2] <- dados[2,2] -1500
+
+# Carol foi contratada por uma multinacional e começou a ganhar 20.000 mensais
+dados[3,3] <- 20000
+
+#Vamos adicionar um quarto indivíduo no banco
+
+#Nome
+dados[4,1] <- "Thiago"
+#Gasto
+dados[4,2] <- 10000
+#Renda
+dados[4,3] <- 100000
+#Curso
+dados[4,4] <- TRUE + TRUE
+#Time
+dados[4,5] <- 3
+
+# Ajuste nos tipos
+
+dados[,4] <- as.logical(dados[,4])
+
+# Inserindo informações pelo console (chamar um parente para cobaia)
+#Nome
+
+dados[5,1] <- readline()
+#Gasto
+dados[5,2] <- readline()
+#Renda
+dados[5,3] <- readline()
+#Curso
+dados[5,4] <- readline()
+#Time
+dados[5,5] <- readline()
+
+# Ajuste nos tipos
+dados[,1] <- as.character(dados[,1])
+dados[,2] <- as.numeric(dados[,2])
+dados[,3] <- as.numeric(dados[,3])
+dados[,4] <- as.logical(dados[,4])
+dados[,5] <- as.factor(dados[,5])
 
 # Iremos querer ver renda comprometida
+
+dados[,6] <- dados[,2]/dados[,3]*100
+dados[,6] <- round(dados[,2]/dados[,3]*100,2)
+
+dados[5,3] <- 1
+
+dados[,6] <- dados[,2]/dados[,3]*100
+dados[,6] <- round(dados[,2]/dados[,3]*100,2)
+
+colnames(dados)
+
+colnames(dados) <- c("Nome","Gastos","Renda","Curso","Time","Renda à Comprometida")
+
+
+
